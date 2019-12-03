245c9ef419ed1683f2263a767b92e3415085dcd7
tanguy <tanguy.brousseau@gmail.com>
2019-12-04 00:41:22 +0100

    correction security 2

--- src/main/java/service/BookServiceImpl.java
+++ src/main/java/service/BookServiceImpl.java
@@ -1,4 +1,45 @@
 package service;
 
+import dao.BookDao;
+import model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 public class BookServiceImpl {
+
+    @Service
+    @Transactional(readOnly = true)
+    public class BookServiceImp implements BookService {
+
+        @Autowired
+        private BookDao bookDao;
+
+        @Transactional
+        public long save(Book book) {
+            return bookDao.save(book);
+        }
+
+        public Book get(long id) {
+            return bookDao.get(id);
+        }
+
+
+        public List<Book> list() {
+            return bookDao.list();
+        }
+
+        @Transactional
+
+        public void update(long id, Book book) {
+            bookDao.update(id, book);
+        }
+
+        @Transactional
+        public void delete(long id) {
+            bookDao.delete(id);
+        }
+    }
 }
